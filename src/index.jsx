32e1c2aabf826cb7423900179e9dcdbde14a4d66
Anh Tuan Van <217000+anhtuanvan@users.noreply.github.com>
2023-12-09 23:22:29 +0000

    add a minimal react UI for tests

---
+++ src/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { useWhisper } from './useWhisper'
+import { key } from './secret'
+
+function App() {
+  const {
+    recording,
+    speaking,
+    transcribing,
+    transcript,
+    pauseRecording,
+    startRecording,
+    stopRecording,
+  } = useWhisper({
+    apiKey: key,
+  })
+
+  return (
+    <div>
+      <p>Recording: {recording}</p>
+      <p>Speaking: {speaking}</p>
+      <p>Transcribing: {transcribing}</p>
+      <p>Transcribed Text: {transcript.text}</p>
+      <button onClick={() => startRecording()}>Start</button>
+      <button onClick={() => pauseRecording()}>Pause</button>
+      <button onClick={() => stopRecording()}>Stop</button>
+    </div>
+  )
+}
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
